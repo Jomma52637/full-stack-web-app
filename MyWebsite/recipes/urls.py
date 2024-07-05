3e346f27494646ec27fb42df6b826466a1d30481
Jomma52637 <jamesdoesjavascript@gmail.com>
2024-07-05 12:30:49 -0500

    Version 1.0.0, base website fully functional

--- MyWebsite/recipes/urls.py
+++ MyWebsite/recipes/urls.py
@@ -9,6 +9,8 @@ from . import views
 urlpatterns = [
     path("", views.RecipesViewList.as_view(), name="recipes-home"),
     path("recipe/<int:pk>", views.RecipeDetailList.as_view(), name="recipes-detail"),
+    path("recipe/<int:pk>/update", views.RecipeUpdateView.as_view(), name="recipes-update"),
+    path("recipe/<int:pk>/delete", views.RecipeDeleteView.as_view(), name="recipes-delete"),
     path("recipe/create", views.RecipeCreateView.as_view(), name="recipes-create"),
     path("about/", views.About, name="recipes-about")
 ]
